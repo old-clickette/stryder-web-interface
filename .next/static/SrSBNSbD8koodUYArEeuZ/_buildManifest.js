505617d5c64da6162a77eabd128724185ec2e6a9
ClaytonTDM <claytontdm@gmail.com>
2023-03-19 05:18:14 +0000

    New Build

---
+++ .next/static/SrSBNSbD8koodUYArEeuZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/794-f8c89a5f904ec0a7.js","static/css/2a12b5a3c8737271.css","static/chunks/pages/index-eb715749ae093b58.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
