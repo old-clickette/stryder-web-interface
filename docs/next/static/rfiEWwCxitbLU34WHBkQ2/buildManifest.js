8bb169e7b931757bb45ae0177d232bfa5f29487a
ClaytonTDM <claytontdm@clickette.net>
2023-03-19 08:58:11 +0000

    New Build

---
+++ docs/next/static/rfiEWwCxitbLU34WHBkQ2/buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/794-de749b8274b655da.js","static/css/2a12b5a3c8737271.css","static/chunks/pages/index-eb715749ae093b58.js"],"/_error":["static/chunks/pages/error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
